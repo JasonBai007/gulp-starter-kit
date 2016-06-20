28e673d1667e9ed4c1596d579c5e068d90984ece
JasonBai007 <byygkcg@163.com>
2016-06-20 21:56:44 +0800

    升级

--- dist/js/buddle.js
+++ dist/js/buddle.js
@@ -2,10 +2,6 @@
 var height = window.innerHeight;
 document.querySelector('body').style.height = height + 'px';
 
-// 背面图片预加载
-var preImg = new Image();
-preImg.src = './dist/img/babala.jpg';
-
 var music = document.querySelector('#music'); 
 var girl = document.querySelector('#girl'); 
 var on = document.querySelector('.on'); 
@@ -13,7 +9,13 @@ var on = document.querySelector('.on');
 // 女孩事件
 girl.addEventListener('click',function(e) {
 	e.stopPropagation();
-	this.classList.toggle('roll');
+	if (this.classList.contains('stop')) {
+		this.classList.add('run');		
+		this.classList.remove('stop');
+	} else {
+		this.classList.remove('run');
+		this.classList.add('stop');	
+	}
 	if( music.paused ) {                 
 	    music.play();  
 	}else{
@@ -26,11 +28,15 @@ on.addEventListener('click',function(e) {
 	if( girl.classList.contains('turn') ) {
 		girl.classList.remove('turn');
 		girl.classList.add('turnOff');
+		girl.removeChild(girl.childNodes[0]);
 	} else {
 		girl.classList.remove('turnOff');
 		girl.classList.add('turn');
+		var node=document.createElement("p");
+		var textnode=document.createTextNode("Jason");
+		node.appendChild(textnode);
+		girl.appendChild(node);
 	}
 });
- 
  // 彩蛋
 console.log('被你发现了，哈哈哈 哈哈哈');
