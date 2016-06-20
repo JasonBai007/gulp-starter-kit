c253bdcb5b10488d8737d49bde7672eed6741e81
JasonBai007 <byygkcg@163.com>
2016-06-20 09:49:12 +0800

    优化五

--- README.md
+++ README.md
@@ -1,15 +1,15 @@
 # Profile
 > Target：借助 npm 和 gulp 实现前端开发自动化.  
-> Techs：npm + gulp
+> Techs：html5 + css3 + npm + gulp
 
 ### Features
-* 自动实现：LESS > CSS > autoprefix > Min > Concat
-* 自动实现：JS > Min > Concat
-* 随时监控文件的变化
+* 自动编译：LESS > CSS > Autoprefix > Compression > Concat
+* 自动编译：JS > Compression > Concat
+* 随时监控文件变化
 * 浏览器自动刷新
-* 功能拓展性强
 * 配置文件自由定制
 * 自带静态服务器
+* 可同时生成压缩和非压缩文件
 
 ### Usage
 > 1、全局安装gulp：$ npm i -g gulp  
--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,25 @@
 var gulp = require('gulp');
-var concat = require('gulp-concat');  //合并文件插件
-var connect = require('gulp-connect');  //本地服务器
-// 生成CSS插件
+var connect = require('gulp-connect');  //文件服务器
+var concat = require('gulp-concat');  //合并
+var rename = require('gulp-rename');  //重命名
 var less = require('gulp-less');
 var LessAutoprefix = require('less-plugin-autoprefix');
 var autoprefix = new LessAutoprefix({ browsers: ['Firefox < 20','>5%','ie 8','ie 9'] });
 var cleanCSS = require('gulp-clean-css');
-// 处理JS插件
 var uglify = require('gulp-uglify');
 
-// 配置路径
-var paths = {
+// 配置输入输出路径
+var inputPath = {
 	html:'./app/index.html',
   	less: 'app/src/less/*.less',
   	js: 'app/src/js/*.js'
 };
+var outputPath = {
+    css: 'app/dist/css/',
+    js: 'app/dist/js/'
+};
 
-// 定义默认任务
+// 定义默认任务和监控任务
 gulp.task('default', ['less','js']);
 gulp.task('watchme', ['connect','watch']);
 
@@ -30,31 +33,35 @@ gulp.task('connect', function () {
 
 // 刷新浏览器
 gulp.task('html', function () {
-    gulp.src(paths.html)
+    gulp.src(inputPath.html)
         .pipe(connect.reload());
 });
 
 // 定义处理LESS任务
 gulp.task('less', function() {
-	return  gulp.src(paths.less)
+	return  gulp.src(inputPath.less)
 			.pipe(less({ plugins: [autoprefix] }))
-  			.pipe(cleanCSS())
-  			.pipe(concat('buddle.min.css'))
-			.pipe(gulp.dest('app/dist/css'))
+  			.pipe(concat('buddle.css'))
+            .pipe(gulp.dest(outputPath.css))
+            .pipe(cleanCSS())
+            .pipe(rename( {extname: '.min.css'} ))
+			.pipe(gulp.dest(outputPath.css))
 			.pipe(connect.reload());
 });
 // 定义处理JS任务
 gulp.task('js', function() {
-	return  gulp.src(paths.js)
-			.pipe(uglify())
-			.pipe(concat('buddle.min.js'))
-			.pipe(gulp.dest('app/dist/js'))
+	return  gulp.src(inputPath.js)
+			.pipe(concat('buddle.js'))
+            .pipe(gulp.dest(outputPath.js))
+            .pipe(uglify())
+            .pipe(rename( {extname: '.min.js'} ))
+			.pipe(gulp.dest(outputPath.js))
 			.pipe(connect.reload());
 });
 
 // 监控任务
 gulp.task('watch',function() {
-	gulp.watch(paths.html, ['html']);
-	gulp.watch(paths.less,['less']);
-	gulp.watch(paths.js,['js']);
+	gulp.watch(inputPath.html, ['html']);
+	gulp.watch(inputPath.less,['less']);
+	gulp.watch(inputPath.js,['js']);
 })
